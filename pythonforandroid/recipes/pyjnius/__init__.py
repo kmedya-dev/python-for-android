ed40df53132c7487d65ff3bf679256248805c8c4
kmedya-dev <koustavamedya.2000@gmail.com>
2025-08-16 17:44:56 +0530

    feat(pyjnius): Update pyjnius version to 1.6.01
    This commit updates the version of the pyjnius recipe from 1.6.1.429 to 1.6.01.

--- pythonforandroid/recipes/pyjnius/__init__.py
+++ pythonforandroid/recipes/pyjnius/__init__.py
@@ -6,7 +6,7 @@ from os.path import join
 
 
 class PyjniusRecipe(PyProjectRecipe):
-    version = '1.6.1.429'
+    version = '1.6.01'
     url = 'https://github.com/kmedya-dev/pyjnius/archive/{version}.zip'
     name = 'pyjnius'
     depends = [('genericndkbuild', 'sdl2', 'sdl3'), 'six']
